f378e960682c3a9ad31cc423c4a7f43619634b37
kalkidanyishak <kalkidanyishak001@gmail.com>
2025-10-30 13:33:34 +0300

    port added

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { errorHandler } from "./middleware/error-handler";
 import relationalEndpoint from "./utils/relational-endpoint";
 
 const app = express();
-const PORT = 3000;
+const PORT =process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
